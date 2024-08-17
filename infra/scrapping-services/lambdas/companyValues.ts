fcb78b47d385a8059c415885ca68e39201329f65
Miguel Obando <miguel.obando204@gmail.com>
2024-08-17 09:52:16 -0500

    WIP 2

--- infra/scrapping-services/lambdas/companyValues.ts
+++
@@ -1,7 +0,0 @@
-import companyValues from './modules/companyValues';
-
-export const handler = async (event: any) => {
-    const URL = 'https://www.linkedin.com/jobs/view/3985173581/'
-    const result =  companyValues(URL);
-    return { statusCode: 200, body: result };
-}
--- infra/scrapping-services/lambdas/modules/companyValues/index.ts
+++
@@ -1,3 +0,0 @@
-export default function companyValues(url: string) {
-    return url;
-}
